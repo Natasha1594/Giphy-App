d25595fdee6eca73ab41b74ba7dacdae218f3b31
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-03-31 16:27:18 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GifComponent } from './gif/gif.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
+import { HttpServiceService }from './service/http-service.service';
+import { StickerServiceService } from './stickerservice/sticker-service.service';
+import { StickersComponent } from './stickers/stickers.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GifComponent,
+    NavbarComponent,
+    SearchComponent,
+    StickersComponent,
+    NotFoundComponent,
+    
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [HttpServiceService,StickerServiceService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/gif/gif.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpServiceService } from '../service/http-service.service'
+import { Giphys } from '../giphys';
+import { error } from 'util';
+
+@Component({
+  selector: 'app-gif',
+  templateUrl: './gif.component.html',
+  styleUrls: ['./gif.component.css']
+})
+export class GifComponent implements OnInit {
+gifs:Giphys [];
+  constructor( public httpService:HttpServiceService) { }
+
+  ngOnInit() {
+    this.searchGif('car');
+  }
+  searchGif(searchIt){
+    this.httpService.searchaGif(searchIt).then(
+      (results)=>{
+        this.gifs = this.httpService.gifys;
+      },
+      (error)=>{
+        console.log('error')
+      }
+     )
+    // function success() {
+    // this.gifs = this.httpService.gifys;
+    // }
+    // function fail(){
+    //   console.log('error')
+    // }
+  }
+ 
+  
+
+}
